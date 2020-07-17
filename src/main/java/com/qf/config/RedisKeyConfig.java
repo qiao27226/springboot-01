22c91830e587123abf67b2f566c1957dbd288c76
qiao <2722680320@qq.com>
2020-07-17 21:36:48 +0800

    确定版本项目架构

--- src/main/java/com/qf/config/RedisKeyConfig.java
+++ src/main/java/com/qf/config/RedisKeyConfig.java
@@ -6,9 +6,16 @@ package com.qf.config;
  */
 public class RedisKeyConfig {
     //令牌
-    public static final String PHONE_TOKEN="voter:phone:";//后面追加手机号 值存储令牌
-    public static final String TOKEN_USER="voter:token:";//后面追加令牌 值存储用户信息
+    public static final String PHONE_TOKEN="shopping:phone:";//后面追加手机号 值存储令牌
+    public static final String TOKEN_USER="shopping:token:";//后面追加令牌 值存储用户信息
+
+    //
+    public static final String PHONE_FOR="shopping:for:phone:";//后面跟手机号 冻结的账号
+    //记录登录输错的手机号，在10分钟内有效期，只要次数超过三次name就会将手机号进行封号处理
+    public static final String PHONE_ERROR="shopping:error:phone:";//后面跟手机号和时间戳
 
     //有效期
     public static final int TOKEN_TIME=30*60; //令牌有效期 单位秒
+    public static final int TOKENFOR_TIME=30*60; //冻结账号 单位秒
+    public static final int PHONERROR_TIME=10*60; //错误的有效期 单位秒
 }
--- src/main/java/com/qf/controller/UserController.java
+++ src/main/java/com/qf/controller/UserController.java
@@ -1,24 +1,26 @@
 package com.qf.controller;
 
+import com.qf.note.CheckToken;
 import com.qf.pojo.TbUser;
 import com.qf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.List;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("api/user/")
 public class UserController {
     @Autowired
     UserService userService;
     @RequestMapping("getAll")
+    @CheckToken
     public List<TbUser> getAll(){
         return userService.getAll();
     }
-    @RequestMapping("tiaozhaun")
-    public String tiaozhaun(){
-        return "pagehome/index.html";
-    }
+
+
+
 }
